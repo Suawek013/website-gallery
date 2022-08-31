3e2954fd2bdce9da6345a036128db2a33e0a32ec
suawek013 <sojkaslawek123@gmail.com>
2022-09-01 00:04:05 +0200

    ad content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import { AwesomeButton } from "react-awesome-button";
+// import "react-awesome-button/dist/styles.css";
+// import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header>
+					<nav>
+						<ul>
+							<li>
+								{/* <AwesomeButton
+									cssModule={AwesomeButtonStyles}
+									type="primary"
+									href="https://google.com"
+									target="_blank"
+								>
+									Home
+								</AwesomeButton> */}
+							</li>
+							<li>
+								<a>item1</a>
+							</li>
+							<li>
+								<a>item1</a>
+							</li>
+							<li>
+								<a>item1</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<main>
+					<div className="image">
+						<img src="./assets/free_time/groupimage.jpg" alt="project" />
+					</div>
+					<div className="image-description">
+						<p>Description</p>
+					</div>
+				</main>
+				{/* <footer>Sławomir Sojka 2022</footer> */}
+			</div>
+		);
+	}
 }
 
 export default App;
