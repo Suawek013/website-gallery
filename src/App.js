23399fcebe6fd0d0515e119a07ba3ad2a5a966db
MWU-013 <mwu-013@android-f14ba10c22da1e03.localdomain>
2022-09-01 12:13:57 +0200

    add photo slider

--- src/App.js
+++ src/App.js
@@ -1,66 +1,69 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import SimpleImageSlider from "react-simple-image-slider";
 
+export default function App(){
 
-// import { AwesomeButton } from "react-awesome-button";
-// import "react-awesome-button/dist/styles.css";
-// import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+	const [images, setImages] = useState([{ url: "images/unsamples/1.jpg" },
+		{ url: "images/unsamples/2.jpg" },
+		{ url: "images/unsamples/3.jpg" },])
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header>
-					<nav>
-						<ul>
-							<li>
-								{/* <AwesomeButton
-									cssModule={AwesomeButtonStyles}
-									type="primary"
-									href="https://google.com"
-									target="_blank"
-								>
-									Home
-								</AwesomeButton> */}
-								<Button class="button-shadow" size="lg" variant="primary">Home</Button>{' '}
-
-								 
-							</li>
-							<li>
-								<DropdownButton size="lg" id="dropdown-basic-button" title="Practice">
-									<Dropdown.Item href="#/action-1">1 Week</Dropdown.Item>
-									<Dropdown.Item href="#/action-2">2 Week</Dropdown.Item>
-								</DropdownButton>
-							</li>
-							<li>
-								<DropdownButton size="lg" id="dropdown-basic-button" title="Culture">
-									<Dropdown.Item href="#/action-1">Leipzig</Dropdown.Item>
-									<Dropdown.Item href="#/action-2">Berlin</Dropdown.Item>
-								</DropdownButton>
-							</li>
-							<li>
-								<Button size="lg" variant="primary">Free time</Button>{' '}
-							</li>
-						</ul>
-					</nav>
-				</header>
-				<main>
-					<div className="image-container">
-						<img src="/images/free_time/groupimage.jpg" alt="project" />
-						{/* <img src="/images/unsamples/XqNsBvVZPps.jpg" alt="project"/> */}
-					</div>
-					<div className="image-description">
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. At vero eos et accusam et justo duo dolores et ea rebum.</p>
-					</div>
-				</main>
-				{/* <footer>Sławomir Sojka 2022</footer> */}
-			</div>
-		);
+	const switchPage = (pageName) => {
+		
+		setImages([
+			{ url: `images/${pageName}/1.jpg` },
+			{ url: `images/${pageName}/2.jpg` },
+			{ url: `images/${pageName}/3.jpg` },
+		  ]);
 	}
-}
 
-export default App;
+	return (
+		<div className="App">
+			<header>
+				<nav>
+					<ul>
+						<li>
+							<Button onClick={()=>switchPage("home")} className="button-shadow" size="lg" variant="primary">Home</Button>{' '}
+						</li>
+						<li>
+							<DropdownButton  size="lg" id="dropdown-basic-button" title="Practice">
+								<Dropdown.Item onClick={()=>switchPage("practice/1week")}>1 Week</Dropdown.Item>
+								<Dropdown.Item onClick={()=>switchPage("practice/2week")}>2 Week</Dropdown.Item>
+							</DropdownButton>
+						</li>
+						<li>
+							<DropdownButton size="lg" id="dropdown-basic-button" title="Culture">
+								<Dropdown.Item onClick={()=>switchPage("culture/leipzig")}>Leipzig</Dropdown.Item>
+								<Dropdown.Item onClick={()=>switchPage("culture/berlin")}>Berlin</Dropdown.Item>
+							</DropdownButton>
+						</li>
+						<li>
+							<Button onClick={()=>switchPage("free_time")} className="button-shadow" size="lg" variant="primary">Free time</Button>{' '}
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<div className="image-container">
+					{/* <img src={`/images/${page}/${counter}.jpg`} alt={`img-${counter}`}></img> */}
+					<SimpleImageSlider
+						autoPlay={true}
+						width={700}
+						height={450}
+						images={images}
+						showBullets={true}
+						showNavs={true}
+					/>
+				</div>
+				<div className="image-description">
+					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+				</div>
+			</main>
+			{/* <footer>Sławomir Sojka 2022</footer> */}
+		</div>
+	);
+}
